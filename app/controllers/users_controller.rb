7db6b7f9fe605c3dbc5070fb238bbe265c108bb8
Tak <takuno919@gmail.com>
2017-10-13 09:47:10 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,11 @@ class UsersController < ApplicationController
   end
   
   def create
-  @user = User.new(user_params)
-  if @user.save
-    flash[:success] = "Welcome to the Sample App!"
-    redirect_to @user
-  else
+   @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
       render 'new'
     end
   end
