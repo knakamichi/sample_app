df770cfd8f4b3220af45d8499e9e316aa118b1bd
Tak <takuno919@gmail.com>
2017-10-13 05:10:21 +0000

    Finish layout and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-  def hello
-      render html: "hello, world!"
+  def home
+  end
+  
+  def help
+  end
+  
+  def about
   end
 end
